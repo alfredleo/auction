5f25e6cdab823e899676ce2652c62500438f4007
Alfred <alfredleo@gmail.com>
2022-07-15 14:39:41 +0500

    fix php-fpm run

--- site/public/index.php
+++ site/public/index.php
@@ -1,13 +1,6 @@
 <?php
 
-$str = "      --accept-license    Accept using a third party scanning provider
---dependency-tree   Show dependency tree with scan results
---exclude-base      Exclude base image from vulnerability scanning (requires --file)
--f, --file string       Dockerfile associated with image, provides more detailed results
---group-issues      Aggregate duplicated vulnerabilities and group them to a single one (requires --json)
---json              Output results in JSON format
---login             Authenticate to the scan provider using an optional token (with --token), or web base token if empty
---reject-license    Reject using a third party scanning provider Ну привет и пока fdasf";
+$str = "ls -lah";
 //write a function to reverse a utf8 string
 function utf8_strrev($str) {
     preg_match_all('/./us', $str, $ar);
